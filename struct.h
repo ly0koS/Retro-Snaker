a4f245c4879251bac6a9101a3287c088e795f852
ly0koS <ly0koS@hotmail.com>
2016-11-20 10:44:54 +0800

    first step create main file&struct.h struct is the file which define the framework and the snake.

---
+++ struct.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<stdio.h>
+#include<time.h>
+#include<Windows.h>
+#include<conio.h>
+#define MAX_WIDE 500
+#define MAX_LENTH 500
+void snake()
+{
+	int len;
+	int body[MAX_WIDE*MAX_LENTH]
+}
+void draw()
+{
+
+}
