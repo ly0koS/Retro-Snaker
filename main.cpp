4cb9f54676c7a47c760636fdea5b80a4ef76f088
ly0koS <ly0koS@hotmail.com>
2016-11-20 11:39:43 +0800

    build the framework

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include"struct.h"
 int main()
 {
-
+	draw();
 }
--- struct.h
+++ struct.h
@@ -3,14 +3,28 @@
 #include<time.h>
 #include<Windows.h>
 #include<conio.h>
-#define MAX_WIDE 500
-#define MAX_LENTH 500
+#define MAX_WIDE 50
+#define MAX_LENTH 20
 void snake()
 {
 	int len;
-	int body[MAX_WIDE*MAX_LENTH]
+	int body[MAX_WIDE*MAX_LENTH];
 }
 void draw()
 {
-
+	char fram[50][50];
+	int i,j;
+	for (i = 0, j = 0; i < 50 && j < 50; i++, j++)
+	{
+		if (i == 0 && j < 50||i==49&&j<50||i<50&&j==0||j==49&&i<50)
+			fram[i][j] = '¡ö';
+		if (!(i == 0 && j < 50 || i == 49 && j < 50 || i < 50 && j == 0 || j == 49 && i < 50))
+		{
+			fram[i][j] = ' ';
+		}
+	}
+	for (i = 0, j = 0; i < 50 && j < 50; i++, j++)
+	{
+		
+	}
 }
